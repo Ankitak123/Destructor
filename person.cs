0aa4af124bf23d25a7579fc454a7126c821f97e0
Ankesh <Ankesh@LENOVO>
2021-09-01 22:29:10 +0530

    Add project files.

---
+++ person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Destructor
+{
+    class DestructorDemo
+    {
+        public DestructorDemo()
+        {
+            Console.WriteLine("constructor object created");
+        }
+        ~DestructorDemo()
+        {
+            Console.WriteLine("object is destroyed");
+        }
+    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            DestructorDemo obj1 = new DestructorDemo();
+            DestructorDemo obj2 = new DestructorDemo();
+            obj1 = null;
+            obj2 = null;
+            GC.Collect();
+            Console.ReadKey();
+        }
+    }
+}
+
